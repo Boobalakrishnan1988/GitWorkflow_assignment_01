bec74487e589c811a66518e7ae0be72a9e2eb93b
Boobalakrishnan1988 <90495799+Boobalakrishnan1988@users.noreply.github.com>
2021-09-11 14:04:09 +0530

    Git Project checked in

---
+++ GitWorkflowDemo/GitWorkflowDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitWorkflowDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! - Main branch");
+        }
+    }
+}
